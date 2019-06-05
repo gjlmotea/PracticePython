3a1e390d0304a4d2b32d072cbc47aafa36fbd361
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-06-05 19:39:38 +0800

    Create amplify_v1.py

---
+++ ch10/07/amplify_v1.py
@@ -0,0 +1,37 @@
+symbol_dict={
+'0':['0000','0  0','0  0','0  0','0000'],
+'1':['  1 ',' 11 ','  1 ','  1 ',' 111'],
+'2':[' 22 ','2  2','  2 ',' 2  ','2222'],
+'3':['3333','   3',' 333','   3','3333'],
+'4':['4  4','4  4','4444','   4','   4'],
+'5':['5555','5   ','5555','   5','5555'],
+'6':['6666','6   ','6666','6  6','6666'],
+'7':['7777','   7','  7 ',' 7  ','7   '],
+'8':['8888','8  8','8888','8  8','8888'],
+'9':['9999','9  9','9999','   9','9999']
+}
+
+
+input_digit = input("請輸入0~9: \n")
+print("\n\n\n")
+
+digit_symbol_list_0 = []
+digit_symbol_list_1 = []
+digit_symbol_list_2 = []
+digit_symbol_list_3 = []
+digit_symbol_list_4 = []
+
+for i in range(int(input_digit)):
+    for j in range(int(input_digit)):
+        print(symbol_dict [str(input_digit)] [0], sep='')
+    for j in range(int(input_digit)):
+        print(symbol_dict [str(input_digit)] [1], sep='')
+    for j in range(int(input_digit)):
+        print(symbol_dict [str(input_digit)] [2], sep='')
+    for j in range(int(input_digit)):
+        print(symbol_dict [str(input_digit)] [3], sep='')
+    for j in range(int(input_digit)):
+        print(symbol_dict [str(input_digit)] [4], sep='')
+
+## 倍數方法印出，將每行每列都放大N倍
+## 但結果還不是想要的

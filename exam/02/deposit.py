84ce02cac6a81d4a58b6bc421628192d69955a22
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-06-13 14:28:32 +0800

    Update deposit.py

--- exam/02/deposit.py
+++ exam/02/deposit.py
@@ -19,7 +19,7 @@ class Deposit():
 
 p = input("請輸入本金(ex:100):")
 r = input("請輸入年利率(ex:0.01):")
-t = input("請輸入存款年數(ex:10):")
+t = input("請輸入存款月份數(ex:12):")
 account = Deposit()
 account.assign(p, r, t)
 

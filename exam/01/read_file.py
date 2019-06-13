e5507ed6701eb42f820557bfa865b7e3b62140ff
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-06-13 14:12:37 +0800

    Update read_file.py

--- exam/01/read_file.py
+++ exam/01/read_file.py
@@ -1,8 +1,9 @@
-#需將file.dat 與 read_file.py放到同一個資料夾
+try:
+    fileObj = open("./file.dat","r", encoding="utf-8")
 
-fileObj = open("file.dat","r", encoding="utf-8")
-
-for line in fileObj:
-    print(line, end='')
-    
-fileObj.close()
+    for line in fileObj:
+        print(line, end='')
+        
+    fileObj.close()
+except Exception as e:
+    print("無法開啟檔案", e)
